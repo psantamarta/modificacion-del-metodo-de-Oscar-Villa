baf339da385e1c3265ba86926f5a417d168baba0
OscarVilFer <comunitoz@gmail.com>
2019-02-06 12:27:17 +0100

    Commit 05. Ahora se pueden añadir participantes

--- Campeonato.java
+++ Campeonato.java
@@ -1,5 +1,10 @@
 import java.util.ArrayList;
 public class Campeonato{
     private Calistenicos participante;
-    private ArrayList<Calistenicos> listaParticipanetes;
+    private ArrayList<Calistenicos> listaParticipantes;
+
+    public void anadirParticipante(int codigoDeIdentificacion, String nombre, int alturaEnCentimetros, double pesoEnKilos){
+        Calistenicos nuevoParticipante = new Calistenicos(codigoDeIdentificacion, nombre, alturaEnCentimetros, pesoEnKilos);
+        listaParticipantes.add(nuevoParticipante);
+    }
 }
