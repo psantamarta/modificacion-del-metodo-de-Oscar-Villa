b2328fea8531a20f6cd5ef3bd4cd9e521f627569
OscarVilFer <comunitoz@gmail.com>
2019-02-06 12:13:19 +0100

    Commit 02

--- Calistenicos.java
+++ Calistenicos.java
@@ -10,4 +10,28 @@ public class Calistenicos{
         peso = pesoEnKilos;
         tieneEquipo = false;
     }
+    
+    public boolean getTieneEquipo(){
+        return tieneEquipo;
+    }
+    
+    public void setTieneEquipo(boolean tieneEquipo){
+        this.tieneEquipo = tieneEquipo;
+    }
+    
+    public int getAltura(){
+        return altura;
+    }
+    
+    public void setAltura(int alturaEnCentimetros){
+        altura = alturaEnCentimetros;
+    }
+    
+    public double peso(){
+        return peso;
+    }
+    
+    public void setPeso(double pesoEnKilos){
+        peso = pesoEnKilos;
+    }
 }
